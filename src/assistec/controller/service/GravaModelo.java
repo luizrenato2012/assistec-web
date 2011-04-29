e004b4cdba7ba8d32da57d80ff488353ce0ada3c
Luiz Renato <luizrenato2005@gmail.com>
2011-04-29 18:44:20 -0300

    rectoring controllers criacao services

--- src/assistec/controller/service/GravaModelo.java
+++ src/assistec/controller/service/GravaModelo.java
@@ -6,9 +6,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import assistec.model.bean.Marca;
-import assistec.model.bean.Modelo;
-import assistec.model.dao.GenericHibernateDAO;
+import assistec.model.service.MarcaService;
+import assistec.model.service.ModeloService;
+import assistec.model.service.ServiceException;
 import assistec.util.UtilAssistec;
 
 
@@ -28,44 +28,25 @@ public class GravaModelo implements ServiceITF {
 			return;
 		} 
 		
-		String descricao = request.getParameter("descricao");
-		String idMarca = request.getParameter("marca");
-		Marca marca = null ; 
-		GenericHibernateDAO<Marca> marcaDao =  new GenericHibernateDAO<Marca>(Marca.class);
-		if ( !UtilAssistec.isVazia(idMarca)) {
-			marca = marcaDao.search(Long.parseLong(idMarca));
-		}
-		GenericHibernateDAO<Modelo> dao = new GenericHibernateDAO<Modelo>(Modelo.class);
-		
-		Modelo modelo =null;
-		Long id=null;
-		String aviso = null;
-		if ( request.getParameter("id")==null ||  request.getParameter("id").trim().equals("")) {
-			modelo = new Modelo();
-			modelo.setDescricao(descricao);
-			modelo.setMarca(marca);
-			id = dao.insert(modelo);
-			aviso=id + " criado com sucesso";
-		} else {
-			id = Long.parseLong(request.getParameter("id"));
-			modelo = dao.search(id);
-			if ( modelo==null) {
-				throw new RuntimeException("modelo inválido");
-			}
-			modelo.setDescricao(descricao);
-			modelo.setMarca(marca);
-			
-			dao.update(modelo);
-			aviso = id +" alterado com sucesso";
-		}
-		
 		try {
+			String strIdModelo = request.getParameter("id");
+			Long idModelo = new ModeloService().grava(strIdModelo, request.getParameter("descricao"), 
+					request.getParameter("marca"));
+			String aviso = null;
+			if (strIdModelo ==null ||  strIdModelo.trim().equals("")) {
+				aviso=idModelo + " criado com sucesso";
+			} else {
+				aviso = idModelo +" alterado com sucesso";
+			}
+			
 			request.setAttribute("msgAviso", "Modelo " + aviso );
-			request.setAttribute("marcas" , marcaDao.listAll());
+			request.setAttribute("marcas" , new MarcaService().listaTodas());
 			request.getRequestDispatcher("modelo_pg.jsp").forward(request, response);
 		} catch (ServletException e) {
 			throw new RuntimeException(e);
 		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} catch (ServiceException e) {
 			throw new RuntimeException(e);
 		}
 		
